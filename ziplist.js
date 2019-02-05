a7174a1cc8dd4cbb1a245542e4d7db4c2995c535
Peter Newton <46580447+peter-newton@users.noreply.github.com>
2019-02-04 19:23:44 -1000

    Finished exercise

---
+++ ziplist.js
@@ -0,0 +1,13 @@
+function zipList(data1, data2) {
+  const result = [];
+  for (let i = 0; i < data1.length; i++) {
+    result.push(data1[i], data2[i]);
+  }
+  return result;
+}
+console.log(zipList(['a', 'b', 'c'], [1, 2, 3]));
+
+function zipListTheSimpleWay(data1, data2) {
+  return _.flatten(_.zip(data1, data2));
+}
+console.log(zipListTheSimpleWay(['a', 'b', 'c'], [1, 2, 3]));
